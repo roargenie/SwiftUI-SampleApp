32f62f3ddaf270ae37a8543dae29fd6ca79b3aff
roargenie <lazyhoper987@gmail.com>
2023-12-14 22:33:17 +0900

    git init

---
+++ SwiftUI_SampleApp/SwiftUI_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SampleAppApp.swift
+//  SwiftUI_SampleApp
+//
+//  Created by 이명진 on 2023/12/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
